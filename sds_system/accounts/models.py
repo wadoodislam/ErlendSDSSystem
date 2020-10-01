1c26bd0a0b68b02cbb90c828b8ea46d227385697
rafayjaved007 <rafay636@gmail.com>
2020-10-01 17:28:31 +0500

    Authentication system updated according to the first review

--- sds_system/accounts/models.py
+++ sds_system/accounts/models.py
@@ -1,1 +1,9 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField('email address', unique=True, help_text='Required, Add a valid email address')
+
+    def __str__(self):
+        return self.username
--- sds_system/accounts/urls.py
+++ sds_system/accounts/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
 ]
