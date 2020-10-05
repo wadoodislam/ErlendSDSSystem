631b424d1f1764888e8b6adb926ecb8746f3a5bc
Junaid <junaid.ikhlaq7@gmail.com>
2020-10-05 21:27:47 +0500

    tonne_dental and ultradent are working fine

--- sds_scraper/sds_scraper/pipelines.py
+++ sds_scraper/sds_scraper/pipelines.py
@@ -12,7 +12,7 @@ from scrapy.pipelines.files import FilesPipeline
 
 class FileDownloadPipeline(FilesPipeline):
     def file_path(self, request, response=None, info=None):
-        original_path = super(FileDownloadPipeline, self).file_path(request, response=None, info=None)
+        # original_path = super(FileDownloadPipeline, self).file_path(request, response=None, info=None)
         # sha1_and_extension = original_path.split('/')[1]  # delete 'full/' from the path
         return request.meta.get('manufacturer', '') + "/" + request.meta.get('filename', '') + ".pdf"
 
@@ -20,4 +20,3 @@ class FileDownloadPipeline(FilesPipeline):
         file_url = item['file_urls'][0]
         meta = {'filename': item['file_display_name'], 'manufacturer': item['manufacturer']}
         yield Request(url=file_url, meta=meta)
-
--- sds_scraper/sds_scraper/spiders/ultradent.py
+++ sds_scraper/sds_scraper/spiders/ultradent.py
@@ -1,4 +1,3 @@
-import os
 from datetime import datetime
 
 import scrapy
@@ -11,10 +10,9 @@ from ..items import SdsScraperItem
 class UltradentSpider(CrawlSpider):
     name = "ultradent"
     base_url = 'https://www.ultradent.com'
-    FILES_STORE = '/Users/junaidikhlaq/PycharmProjects/ErlendSDSSystem/sds_scraper/sds_scraper/sds_data/Ultradent'
 
     def start_requests(self):
-        urls = ['https://www.ultradent.com/resources/safety-data-sheets', ]
+        urls = ['https://www.ultradent.com/resources/safety-data-sheets']
         for url in urls:
             yield scrapy.FormRequest(url=url,
                                      method='POST',
@@ -22,29 +20,22 @@ class UltradentSpider(CrawlSpider):
                                      callback=self.parse)
 
     def parse(self, response, **kwargs):
-        item = SdsScraperItem()
         for data in response.css('ul.linkList li').getall():
             name = Selector(text=data).css('a::text').get()
             url = Selector(text=data).css('a::attr(href)').get()
+            item = SdsScraperItem()
+            item['source'] = 'ultradent.py'
+            item['manufacturer'] = 'Ultradent Products, Inc'
+            item['file_urls'] = [self.base_url + url]
+            item['file_display_name'] = name
+            item['crawl_date'] = datetime.now().date().strftime('%d.%m.%Y')
 
-            if not os.path.isdir(self.FILES_STORE):
-                os.mkdir(self.FILES_STORE)
             yield scrapy.Request(
                 url=response.urljoin(url),
-                callback=self.save_pdf
+                callback=self.parse_pdf,
+                meta={'item': item}
             )
 
-            # item['source'] = 'ultradent.py'
-            # item['manufacturer'] = 'Ultradent Products, Inc'
-            # item['file_urls'] = self.base_url + url
-            # item['file_display_name'] = name
-            # item['crawl_date'] = datetime.now().date().strftime('%d.%m.%Y')
-            #
-            # yield item
-
-    def save_pdf(self, response):
-        path = response.url.split('/')[-1]
-        complete_name = os.path.join(self.FILES_STORE, path)
-        self.logger.info('Saving PDF %s', complete_name)
-        with open(complete_name, 'wb') as f:
-            f.write(response.body)
+    def parse_pdf(self, response):
+        item = response.meta.get('item', '')
+        yield item
