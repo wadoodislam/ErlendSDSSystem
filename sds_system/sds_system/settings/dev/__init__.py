a3e02c22a5534d420b36da8177987915d6625702
wadood <wadoodislam@gmail.com>
2021-01-19 19:59:06 +0500

    added migrations

---
+++ sds_system/sds_system/settings/dev/__init__.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+MACHINE_URL = f'http://{ALLOWED_HOSTS[0]}:8080/'
