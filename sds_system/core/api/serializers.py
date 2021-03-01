ea5b8fbcfbf91711434ec8bc7363329301fd8820
wadood <wadoodislam@gmail.com>
2021-03-02 01:41:49 +0500

    corrected the sds path

--- sds_system/core/api/serializers.py
+++ sds_system/core/api/serializers.py
@@ -38,7 +38,7 @@ class SDSPDFSerializer(serializers.ModelSerializer):
 
     def validate(self, attr):
         attr['pdf_md5'] = md5hash(attr['sds_product_name'], attr['sds_harvest_source'])
-        attr['sds_download_url'] = f"{settings.MACHINE_URL}:8080/media/sds/{attr['sds_harvest_source']}/sds/{attr['name']}".replace(' ', '%20')
+        attr['sds_download_url'] = f"{settings.MACHINE_URL}:8080/media/sds/{attr['sds_harvest_source'].id}/sds/{attr['name']}".replace(' ', '%20')
         attr['language'] = Language.objects.get(name=attr['language'])
         attr['manufacturer'] = Manufacturer.objects.get(name=attr['manufacturer'])
 
