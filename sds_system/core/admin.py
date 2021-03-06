691a073482d2c8b208f64c00e1a81ab2470b2f22
wadood <wadoodislam@gmail.com>
2021-03-06 16:29:05 +0500

    run hiding

--- sds_system/core/admin.py
+++ sds_system/core/admin.py
@@ -62,9 +62,10 @@ class SDSHarvestSourceAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
     def link_to_actions(self, obj):
-        if ' ' in obj.id:
-            return format_html('<a href="{}" target="_blank">Run</a>', reverse("run", args=[obj.id[:obj.id.index(' ')]]))
-        return format_html('<a href="{}" target="_blank">Run</a>', reverse("run", args=[obj.id]))
+        if obj.status == SDSHarvestSource.STATUS.DEVELOPED.value and obj.method == SDSHarvestSource.METHOD.SCRAPING.value:
+            if ' ' in obj.id:
+                return format_html('<a href="{}" target="_blank">Run</a>', reverse("run", args=[obj.id[:obj.id.index(' ')]]))
+            return format_html('<a href="{}" target="_blank">Run</a>', reverse("run", args=[obj.id]))
 
 
 class SDSHarvestRunAdmin(admin.ModelAdmin):
